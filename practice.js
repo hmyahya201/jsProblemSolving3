626447892bc1bb43664c9780759356f2e3f30e5b
code y <hmyahya001@gmail.com>
2022-09-10 13:23:13 +0600

    js problem solving 2

---
+++ practice.js
@@ -0,0 +1,82 @@
+//1 CONVERT MINUTES TO SECOND
+/*
+let minutes = 75;
+let second = 75*60 + "s";
+console.log(second);
+*/
+
+//2 CONVERT HOURS TO MINUTES
+/*
+let hours = 7;
+let minutes = 7 * 60 + "m";
+console.log(minutes);
+*/
+
+// 3 WRITE BANGLADESH WITH CONDITION
+
+/*
+let a = "Bangla"
+let b = "Desh"
+let n = "h";
+if(n % 3 == 0 && n % 5 == 0){
+    console.log(a + b);  
+}
+else if(n % 3 == 0){
+    console.log(a);  
+}
+else if(n % 5 == 0){
+    console.log(b)
+}
+else{
+    console.log("Your are ruhinga")
+}
+*/
+
+//4 AFTER DISCOUNT PRICE CALCULATION
+
+/*
+let price = 980;
+if(price >= 900){
+    let discount = (price * 17)/100;
+    let pay = price - discount;
+    console.log(`You have to pay ${pay} after 17% discount with free shipping`);
+}
+else if(price >= 500){
+    let discount = (price * 17)/100;
+    let pay = price - discount;
+    console.log(`You have to pay ${pay} without free shipping`); 
+}
+*/
+
+//5 ADDITION OF NUMBER WITH SQUARE
+/*
+let n = 69;
+let sum = 0;
+let result;
+
+for(let i = 1; i <=n; i++){
+    result = i**2
+    sum += result;
+}
+console.log(sum);
+*/
+
+// 6 ADDITION OF THE NUMBER OF THE DIGIT AND DEVIDE IT
+/*
+let num = 80;
+
+let makeString = num.toString()
+let addition1 = makeString[0]
+let addition2 = makeString[1]
+let addition_1 = parseInt(addition1);
+let addition_2 = parseInt(addition2);
+let result = addition_1 + addition_2;
+
+console.log(num % result == 0);
+
+*/
+
+
+
+
+
